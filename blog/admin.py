b7013abf3b84cdec54ae1067b0e1935d05d128ab
Admin <admin@thecodeblogs.com>
2021-02-26 06:19:48 -0500

    Add friendly screens for entire admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,15 @@ from blog.models import ( EntryEnvelope, Profile, Comment, Tag, View,
                          Interaction, VisitorProfile )
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_usable_email', 'id')
+    def get_usable_email(self, obj):
+        return obj.user.email
+
+    get_usable_email.admin_order_field = 'user__email'
+    get_usable_email.short_description = 'Email'
+
+
 class EntryAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['author', 'entry']}),
@@ -13,12 +22,19 @@ class EntryAdmin(admin.ModelAdmin):
                                          'defunct']}),
         ('JSON', {'fields': ['entry_formatted']}),
     ]
-    list_display = ('id', 'entry_id', 'title', 'published', 'defunct')
+    list_display = ('entry_id', 'title', 'get_usable_email', 'published', 'defunct')
     readonly_fields = ('title', 'entry_formatted',
                        'created_on', 'modified_on',
                        'create_date', 'edit_date', 'slug',
                        'defunct')
+    search_fields = ['title', 'author__email']
     ordering = ('-edit_date', 'entry_id')
+
+    def get_usable_email(self, obj):
+        return obj.author.email
+
+    get_usable_email.admin_order_field = 'author__email'
+    get_usable_email.short_description = 'Email'
 
 class VisitorProfileAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -29,10 +45,55 @@ class VisitorProfileAdmin(admin.ModelAdmin):
     readonly_fields = ('telemetry_formatted',)
 
 
-admin.site.register(Profile)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_on', 'get_usable_email', 'get_title')
+    search_fields = ['content']
+
+    def get_title(self, obj):
+        return obj.entry_envelope.title
+
+    get_title.admin_order_field = 'entry_envelope__title'
+    get_title.short_description = 'Title'
+
+    def get_usable_email(self, obj):
+        return obj.user.email
+
+    get_usable_email.admin_order_field = 'user__email'
+    get_usable_email.short_description = 'Email'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'label')
+    search_fields = ['label']
+
+
+class ViewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_usable_email', 'session_uid', 'get_title')
+
+    def get_title(self, obj):
+        return obj.entry_envelope.title
+
+    def get_usable_email(self, obj):
+        if (obj.user is not None):
+            return obj.user.email
+        else:
+            return None
+
+class InteractionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_usable_email', 'session_uid')
+    search_fields = ['content']
+
+    def get_usable_email(self, obj):
+        if (obj.user is not None):
+            return obj.user.email
+        else:
+            return None
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(EntryEnvelope, EntryAdmin)
-admin.site.register(Comment)
-admin.site.register(Tag)
-admin.site.register(View)
-admin.site.register(Interaction)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(View, ViewAdmin)
+admin.site.register(Interaction, InteractionAdmin)
 admin.site.register(VisitorProfile, VisitorProfileAdmin)
